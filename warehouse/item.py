82cedbb9c12578af4a6c7b36ff824c5fd602f2fe
Ethan <eschneider32@gatech.edu>
2024-07-02 18:05:03 -0400

    warehouse mostly implemented

--- warehouse/item.py
+++ warehouse/item.py
@@ -2,19 +2,9 @@ from enum import Enum, auto, unique
 
 @unique
 class ItemCategory(Enum):
+    EMPTY = auto()
     Shoes = auto()
     Jackets = auto()
     Pants = auto()
     Shampoo = auto()
     Soap = auto()
-
-class Item:
-    def __init__(self, itemID: int, item_category: ItemCategory) -> None:
-        self.__itemID = itemID
-        self.category = item_category
-        
-        
-if __name__=="__main__":
-    print(ItemCategory.Shoes.value)
-    print(list(ItemCategory))
-    print(ItemCategory(1).name)
--- warehouse/warehouse.py
+++ warehouse/warehouse.py
@@ -1,7 +1,7 @@
 import numpy as np
 import json
 
-from item import Item, ItemCategory
+from item import ItemCategory
 
 class Warehouse:
     def __init__(self) -> None:
@@ -9,47 +9,66 @@ class Warehouse:
         self.__init_inventory()
         
     def __init_inventory(self):
+        # Defined by current warehouse layout
         max_pos = 2659
         
         for i in range(max_pos + 1):
-            self.__inventory[i] = 0
+            self.__inventory[i] = ItemCategory(1).name
     
     def printInventory(self):
         print(self.__inventory)
         
     def readData(self):
-        pass
+        with open("warehouse/data/file.json", "r", encoding='utf-8') as f:
+            data = json.load(f)
+        self.__inventory = {i: ItemCategory[data[x]].name for i, x in enumerate(data)}
     
     def writeData(self):
-        pass
+        output = {x: self.__inventory[x] for x in range(len(self.__inventory))}
+        with open("warehouse/data/file.json", "w+", encoding='utf-8') as f:
+            json.dump(output, f, ensure_ascii=False, indent=4)
         
     def findEmpty(self):
         empty = []
         for i, item in enumerate(self.__inventory):
-            if self.__inventory[item] == 0:
+            if self.__inventory[item] == ItemCategory(1).name:
                 empty.append(i)
         return empty
     
-    def find(self):
-        pass
+    def find(self, target):
+        locations = []
+        for i, item in enumerate(self.__inventory):
+            if self.__inventory[item] == 0:
+                continue
+            if self.__inventory[item] == target:
+                locations.append(i)
+        return locations
     
-    def add(self, pos, item: Item):
+    def add(self, pos, item: str):
         self.__inventory[pos] = item
     
-    def remote(self, pos):
-        pass
+    def remove(self, pos):
+        self.__inventory[pos] = ItemCategory(1).name
     
     
 if __name__=="__main__":
     warehouse = Warehouse()
     
-    positions = list(range(2659+1))
+    # Init Warehouse
+    # positions = list(range(2659+1))
     
-    for i in range(int(len(positions) - np.floor(len(positions)*.2))):
-        pos = np.random.choice(positions, replace=False)
-        positions.pop(positions.index(pos))
-        item = Item(np.random.randint(0, 99999), ItemCategory(np.random.randint(low=1, high=len(ItemCategory)+1)).name)
-        warehouse.add(pos, item)
-    print(warehouse.printInventory())
+    # for i in range(int(len(positions) - np.floor(len(positions)*.2))):
+    #     pos = np.random.choice(positions, replace=False)
+    #     positions.pop(positions.index(pos))
+    #     item = ItemCategory(np.random.randint(low=2, high=len(ItemCategory)+1)).name
+    #     warehouse.add(pos, item)
+    
+    warehouse.readData()
+    
     print(warehouse.findEmpty())
+    
+    print(warehouse.find(ItemCategory(2).name))
+    print(len(warehouse.find(ItemCategory(2).name)))
+    
+    # warehouse.writeData()
     
