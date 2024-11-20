f7b0755f6ac3f93c1cc7b20ada5050f553bd4809
Ethan Schneider <ets1099@yahoo.com>
2024-11-19 21:04:09 -0500

    Deleted Symbotic simulator files, old archived files, etc.

--- Script and documentation(1)/consumer.py
+++
@@ -1,7 +0,0 @@
-from rabbitmq import RabbitMQ
-
-def callback(ch, method, properties, body):
-    print(f"Received {body}")
-
-rabbitmq = RabbitMQ()
-rabbitmq.consume("Symbotic.Routing.RoutingService.1.Queue", callback)
